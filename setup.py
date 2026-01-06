fe78b8e706e38d6028d792b8cc3783882ac5c890
Jayasuryan Mutyala <jsm.dgme@gmail.com>
2026-01-06 18:20:45 +0800

    setup venv

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='medical-chatbot',
+    version="0.1.0",
+    author="Jayasuryan Mutyala",
+    author_email='jsm.dgme@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+    )
